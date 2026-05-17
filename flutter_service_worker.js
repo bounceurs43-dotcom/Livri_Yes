2488d15cc399fb18a4b49302b6cad6d61a660d14
bounceurs43-dotcom <bounceurs43@gmail.com>
2026-05-17 10:54:22 +0100

    Fix admin dashboard live notifications syncing issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -289,14 +289,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf67bfe1307c038a920d6da8106d549f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "93786b4f8225443f5267dea0d0b293f2",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3c54b9a7ea509138564ae2e3beb048a2",
+"flutter_bootstrap.js": "558c81c87aa8d57f514a8da2def992ec",
 "icons/Icon-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "icons/Icon-maskable-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-maskable-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "index.html": "3f6fd1fe10bf252743a738dc3aa063e4",
 "/": "3f6fd1fe10bf252743a738dc3aa063e4",
-"main.dart.js": "bf906e73ee5c3aac9a6c72a7766678db",
+"main.dart.js": "dd5d6655f2ed19e429959780497eda8f",
 "manifest.json": "0b5c95ecfb79686c77fc3b3f4526d675",
 "version.json": "e9eb58db72d407be27e9fa052224c304"};
 // The application shell files that are downloaded before a service worker can
