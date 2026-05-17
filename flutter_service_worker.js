a8ba97d2f62098f8ee87cc1d627c7710e59728d8
bounceurs43-dotcom <bounceurs43@gmail.com>
2026-05-17 19:26:58 +0100

    Update admin: Add Le choix de Livriyes switch in product management UI

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -289,14 +289,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf67bfe1307c038a920d6da8106d549f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "93786b4f8225443f5267dea0d0b293f2",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e7be03282eef3d5fafd80dc57ff7a2ee",
+"flutter_bootstrap.js": "95e5cd8e187f940e1acdfec20febcd93",
 "icons/Icon-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "icons/Icon-maskable-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-maskable-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "index.html": "3f6fd1fe10bf252743a738dc3aa063e4",
 "/": "3f6fd1fe10bf252743a738dc3aa063e4",
-"main.dart.js": "ecd4e1ddbcece839c70a7b2772ea4099",
+"main.dart.js": "86d0db256a2714072ca6af159cd889c2",
 "manifest.json": "0b5c95ecfb79686c77fc3b3f4526d675",
 "version.json": "e9eb58db72d407be27e9fa052224c304"};
 // The application shell files that are downloaded before a service worker can
