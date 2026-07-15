fcd3bcaeb89559d7cde6c1e50e3eca998cc29b93
YourUsername <youremail@example.com>
2026-07-15 18:23:17 +0100

    feat: Add search functionality to email update dialog

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -289,14 +289,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf67bfe1307c038a920d6da8106d549f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "93786b4f8225443f5267dea0d0b293f2",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0fb33febcb4ddf9ed85a40770b082755",
+"flutter_bootstrap.js": "51c21e23e8ea044ed5574f54a0cc165a",
 "icons/Icon-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "icons/Icon-maskable-192.png": "182dd7b1c15ca64707b0fbe7a69c1a05",
 "icons/Icon-maskable-512.png": "fb156f6e3e9183d015144ae144112cfd",
 "index.html": "3f6fd1fe10bf252743a738dc3aa063e4",
 "/": "3f6fd1fe10bf252743a738dc3aa063e4",
-"main.dart.js": "664cc48463bb341515c1777e6e443768",
+"main.dart.js": "aee32b00af2897f229a6c80f350c8e00",
 "manifest.json": "0b5c95ecfb79686c77fc3b3f4526d675",
 "version.json": "e9eb58db72d407be27e9fa052224c304"};
 // The application shell files that are downloaded before a service worker can
